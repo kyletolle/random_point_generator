308286cfa7c04d36a08eddf381dc63d755dad7d5
Kyle Tolle <kyle@nullsix.com>
2015-05-27 21:21:19 -0600

    Use BigDecimal for precision
    This has been sitting around uncommitted for a long time. Worked on
    making all the specs pass, and I want to commit this. Can refactor more
    going forward.

--- lib/random_point_generator.rb
+++ lib/random_point_generator.rb
@@ -1,34 +1,65 @@
-class RandomPointGenerator
-  def initialize(sw_point = nil, ne_point = nil)
-    sw_point ||= [-90.0, -180.0]
-    ne_point ||= [90.0, 180.0]
+require 'bigdecimal'
 
-    @sw_point = sw_point
-    @ne_point = ne_point
+class RandomPointGenerator
+  MAX_DIGITS_OF_PRECISION = 6
+
+  def initialize(sw_point = nil, ne_point = nil)
+    sw_point ||= ["-90.0", "-180.0"]
+    ne_point ||= ["90.0", "180.0"]
+
+    @sw_point = convert_to_precise_point(sw_point)
+    @ne_point = convert_to_precise_point(ne_point)
   end
 
   def random_point
     y_range = (sw_lat .. ne_lat)
     x_range = (sw_lng .. ne_lng)
 
-    [rand(y_range), rand(x_range)]
+    [random_big_decimal(y_range), random_big_decimal(x_range)]
   end
 
 private
   def sw_lat
-    @sw_point[0]
+    @sw_point[0].to_f
   end
 
   def sw_lng
-    @sw_point[1]
+    @sw_point[1].to_f
   end
 
   def ne_lat
-    @ne_point[0]
+    @ne_point[0].to_f
   end
 
   def ne_lng
-    @ne_point[1]
+    @ne_point[1].to_f
+  end
+
+  def convert_to_precise_point(imprecise_point)
+    imprecise_point.map do |value|
+      convert_to_precise_number(value)
+    end
+  end
+
+  def convert_to_precise_number(imprecise_number)
+    # NOTE: Creating a big decimal here with the float itself causes some
+    # rounding issues... strange. So we convert to a string.
+    BigDecimal.new(imprecise_number.to_s, MAX_DIGITS_OF_PRECISION)
+  end
+
+  def random_big_decimal(range)
+    # NOTE: We can't use any of the built-in BigDecimal methods to give us a
+    # FIXED length number of decimals. Not that I've seen, at least.
+    # So we end up doing that truncation ourselves.
+    random_float          = rand(range)
+    random_decimal        = convert_to_precise_number(random_float)
+    decimal_string        = random_decimal.to_s("F")
+    dot_position          = decimal_string.index(".")
+    last_decimal_position = dot_position + MAX_DIGITS_OF_PRECISION + 1
+    if last_decimal_position > decimal_string.length-1
+      last_decimal_position = decimal_string.length
+    end
+    decimal_string.slice(0, last_decimal_position)
   end
 end
 
--- spec/random_point_generator_spec.rb
+++ spec/random_point_generator_spec.rb
@@ -2,20 +2,26 @@ require 'random_point_generator'
 
 describe RandomPointGenerator do
   shared_examples "generates a valid, random point" do
+    let(:max_precision) { RandomPointGenerator::MAX_DIGITS_OF_PRECISION }
+
     it "generates a point" do
       expect(subject).to be_an Array
     end
 
     it "gives a valid, random latitude" do
-      lat = random_point[0]
-      expect(lat).to be >= expected_lower_lat_bound
-      expect(lat).to be <= expected_upper_lat_bound
+      lat = BigDecimal.new(random_point[0])
+      expect(lat).to be >=
+        BigDecimal.new(expected_lower_lat_bound, max_precision)
+      expect(lat).to be <=
+        BigDecimal.new(expected_upper_lat_bound, max_precision)
     end
 
     it "gives a valid, random longitude" do
-      lng = random_point[1]
-      expect(lng).to be >= expected_lower_lng_bound
-      expect(lng).to be <= expected_upper_lng_bound
+      lng = BigDecimal.new(random_point[1])
+      expect(lng).to be >=
+        BigDecimal.new(expected_lower_lng_bound, max_precision)
+      expect(lng).to be <=
+        BigDecimal.new(expected_upper_lng_bound, max_precision)
     end
   end
 
@@ -39,36 +45,61 @@ describe RandomPointGenerator do
 
   describe "with a bounding box" do
     describe "with both sw_point and ne_point" do
-      let(:sw_lat) { 5.0 }
-      let(:sw_lng) { 50.0 }
-
-      let(:sw_point) { [sw_lat, sw_lng] }
-
-      let(:ne_lat) { 53.4 }
-      let(:ne_lng) { 54.87 }
-
-      let(:ne_point) { [ne_lat, ne_lng] }
-
       let(:generator) { described_class.new(sw_point, ne_point) }
       subject         { generator }
 
-      describe "#random_point" do
+      describe "with nice, round numbers" do
+        let(:sw_lat) { 1.0 }
+        let(:sw_lng) { 1.0 }
+
+        let(:sw_point) { [sw_lat, sw_lng] }
+
+        let(:ne_lat) { 2.0 }
+        let(:ne_lng) { 2.0 }
+
+        let(:ne_point) { [ne_lat, ne_lng] }
+
+
+        describe "#random_point" do
+          let(:random_point) { generator.random_point }
+          subject            { random_point }
+
+          let(:expected_lower_lat_bound) { sw_lat }
+          let(:expected_upper_lat_bound) { ne_lat }
+
+          let(:expected_lower_lng_bound) { sw_lng }
+          let(:expected_upper_lng_bound) { ne_lng }
+
+          include_examples "generates a valid, random point"
+        end
+      end
+
+      describe "with more digits of precision than necessary" do
         let(:random_point) { generator.random_point }
         subject            { random_point }
 
-        let(:expected_lower_lat_bound) { sw_lat }
-        let(:expected_upper_lat_bound) { ne_lat }
+        let(:sw_point) { ["1.1111111111", "1.9999999999"] }
+        let(:ne_point) { sw_point }
 
-        let(:expected_lower_lng_bound) { sw_lng }
-        let(:expected_upper_lng_bound) { ne_lng }
+        it "has a latitude with the correct number of decimals" do
+          lat = random_point[0]
+          lat_decimals = lat.split('.').last
 
-        include_examples "generates a valid, random point"
+          expect(lat_decimals.length).to eq RandomPointGenerator::MAX_DIGITS_OF_PRECISION
+        end
+
+        it "has a longitude with the correct number of decimals" do
+          lng = random_point[1]
+          lng_decimals = lng.split('.').last
+
+          expect(lng_decimals.length).to eq RandomPointGenerator::MAX_DIGITS_OF_PRECISION
+        end
       end
     end
 
     describe "with valid sw_point and nil ne_point" do
-      let(:sw_lat) { 5.0 }
-      let(:sw_lng) { 50.0 }
+      let(:sw_lat) { 1.0 }
+      let(:sw_lng) { 1.0 }
 
       let(:sw_point) { [sw_lat, sw_lng] }
 
@@ -96,8 +127,8 @@ describe RandomPointGenerator do
 
       let(:sw_point) { nil }
 
-      let(:ne_lat) { 53.4 }
-      let(:ne_lng) { 54.87 }
+      let(:ne_lat) { 1.0 }
+      let(:ne_lng) { 1.0 }
 
       let(:ne_point) { [ne_lat, ne_lng] }
 
